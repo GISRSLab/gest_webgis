4bb119bbb9cc79ce661f72e17ad4ef38052633d9
zayn <1546492103@qq.com>
2023-10-25 15:29:04 +0800

    fix some

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import { type Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
---
+++ docs/components/FeatureImg.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { withBase } from 'vitepress';
+import { defineProps } from 'vue';
+const props = withDefaults(defineProps<{
+    src: string,
+    alt: string
+}>(),{
+    src:"/imgs/ArcGIS.png",
+    alt:"ArcGIS"
+})
+</script>
+
+<template>
+    <img :src="withBase(props.src)" :alt="props.alt"/>
+</template>
+
+<style scoped>
+
+</style>
