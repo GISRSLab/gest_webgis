27a3c3805651ac6ea4213a5b92b4a9b9bf9754ec
zayn <1546492103@qq.com>
2023-12-21 17:51:30 +0800

    fix git stage issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './docs/*.{html,js,vue,ts,md}',
     './docs/**/*.{html,js,vue,ts,md}',
     './docs/.vitepress/**/*.{html,js,vue,ts,md}'
   ],
