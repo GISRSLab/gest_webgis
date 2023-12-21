5d1933d1c3b490f81e5ec7376145ae8d317e6eac
zayn <1546492103@qq.com>
2023-12-21 22:09:47 +0800

    <add> comments system powered by giscus

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from 'vue'
 import { type Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Giscus from '@giscus/vue';
 import '../custom.css'
 import './style.css'
 import "leaflet/dist/leaflet.css"
@@ -14,6 +15,20 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'doc-after':()=>h(Giscus,{
+        repo: "DSYZayn/gest_webgis",
+        repoId:"R_kgDOKkhevg",
+        category:"Announcements",
+        categoryId:"DIC_kwDOKkhevs4Cb59n",
+        mapping:"pathname",
+        strict:"0",
+        reactionsEnabled:"1",
+        emitMetadata:"0",
+        inputPosition:"top",
+        theme:"preferred_color_scheme",
+        lang:"zh-CN",
+        loading:"lazy"
+      })
     })
   },
   enhanceApp({ app, router, siteData }) {
