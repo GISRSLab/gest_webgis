910b195f5bbeacff07b3d594dd032e4a79f12e77
zayn <1546492103@qq.com>
2023-12-29 17:53:59 +0800

    fix: giscus component key

--- docs/.vitepress/theme/ThemedGiscus.vue
+++ docs/.vitepress/theme/ThemedGiscus.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { ComputedRef, computed } from 'vue';
-import { useData } from 'vitepress';
+import { useData} from 'vitepress';
 import Giscus, { Theme } from '@giscus/vue';
 
-const { isDark } = useData();
+const { isDark, page } = useData();
+
 const giscusTheme:ComputedRef<Theme> = computed(function():Theme{
     return isDark.value ? "dark": "light"
 })
+
 </script>
 
 <template>
     <Giscus
+        :key="page.relativePath"
         repo="DSYZayn/gest_webgis"
         repoId="R_kgDOKkhevg"
         category="Announcements"
         categoryId="DIC_kwDOKkhevs4Cb59n"
         mapping="pathname"
-        strict="0"
+        strict="1"
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="top"
